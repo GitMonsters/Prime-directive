1d70113016c2f535dea5c8a54d4c5b0bd2238d6c
Nave Reseip <nave@stnmanagement.com>
2026-02-09 21:13:37 -0700

    Add GitHub Pages docs workflow, fix .dockerignore, update Homebrew SHA256s

--- Formula/rustyworm.rb
+++ Formula/rustyworm.rb
@@ -16,7 +16,7 @@ class Rustyworm < Formula
   on_macos do
     on_arm do
       url "https://github.com/GitMonsters/Prime-directive/releases/download/v2.0.0/rustyworm-macos-arm64"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "8785f6b7ed28bbe11389befc3576c6ff3539d2eed56347d4a8c8dd52e8d32f0d"
 
       def install
         bin.install "rustyworm-macos-arm64" => "rustyworm"
@@ -25,7 +25,7 @@ class Rustyworm < Formula
 
     on_intel do
       url "https://github.com/GitMonsters/Prime-directive/releases/download/v2.0.0/rustyworm-macos-amd64"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "25ef0f68b4280c78ace598942510507378e02d3f2998b1fe2f4f1fe43636af11"
 
       def install
         bin.install "rustyworm-macos-amd64" => "rustyworm"
@@ -36,7 +36,7 @@ class Rustyworm < Formula
   on_linux do
     on_arm do
       url "https://github.com/GitMonsters/Prime-directive/releases/download/v2.0.0/rustyworm-linux-arm64"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "2037548e42dec7969c6f6040a2286db8b556201cf3336b9a93f1f706d49104cc"
 
       def install
         bin.install "rustyworm-linux-arm64" => "rustyworm"
@@ -45,7 +45,7 @@ class Rustyworm < Formula
 
     on_intel do
       url "https://github.com/GitMonsters/Prime-directive/releases/download/v2.0.0/rustyworm-linux-amd64"
-      sha256 "PLACEHOLDER_SHA256"
+      sha256 "2b519ada372f9d8745f9a7b7df26ea56f72d8721ff4dd5f77cfa0746485f278a"
 
       def install
         bin.install "rustyworm-linux-amd64" => "rustyworm"
